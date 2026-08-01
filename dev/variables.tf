644016adc82bf1348c320093c32a7e3b646571f4
Sankalp2620 <sankalpnandigama.307@gmailcom>
2026-08-01 22:23:25 +0530

    configured the git hub scretes, creating infra only VPC for now

--- dev/variables.tf
+++ dev/variables.tf
@@ -2,31 +2,42 @@ variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
 }
-
+variable "tags" {
+  description = "A map of tags to assign to the resource."
+  type        = map(string)
+  value       = {
+    "Environment" = "dev"
+    "Project"     = "EKS-Cluster-VPC"
+    "name"        = "EKS-Cluster-VPC"
+  }
+}
 variable "region" {
   description = "AWS region"
   type        = string
+  value       = "ap-south-2"
 }
 
 variable "instance_types" {
   description = "List of EC2 instance types for EKS worker nodes"
   type        = list(string)
+  value       = ["t3.medium"]
 }
 
 variable "cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
-  
+  value       = "10.0.0.0/16"
 }
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  
+  value       = ["10.0.1.0/24"]
 }
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
-  
+  value       = ["10.0.2.0/24"]
+
 }
 
 variable "environment" {
